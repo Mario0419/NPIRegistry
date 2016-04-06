3fb112f44c51699c72463eb737d253c7d8a5f913
Mario Massad <Mario Massad>
2016-04-05 21:30:42 -0400

    Improvided detailed dialog

--- app/src/main/java/com/mx3studios/npiregistry/npiDrawerFragment/NpiDetailDialogFragment.java
+++ app/src/main/java/com/mx3studios/npiregistry/npiDrawerFragment/NpiDetailDialogFragment.java
@@ -8,9 +8,12 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.mx3studios.npiregistry.R;
 import com.mx3studios.npiregistry.npi.NpiResult;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Mario on 3/6/2016.
@@ -36,7 +39,40 @@ public class NpiDetailDialogFragment extends DialogFragment{
         fm = getFragmentManager();
         view = inflater.inflate(R.layout.fragment_detail_npi_dialog, null);
         builder.setView(view);
+        initiateComponents();
         return builder.create();
+    }
+
+    private void initiateComponents() {
+        TextView textView = (TextView)view.findViewById(R.id.detailed_name);
+        textView.setText(provider.getBasicInfo().getFullName());
+
+        textView = (TextView)view.findViewById(R.id.detailed_taxonomy);
+        textView.setText(provider.getTaxonomies().get(0).getDesc());
+
+        textView = (TextView)view.findViewById(R.id.detailed_npi);
+        textView.setText(String.valueOf(provider.getNpi()));
+
+        textView = (TextView)view.findViewById(R.id.detailed_address);
+        textView.setText(provider.getAddresses().get(0).getAddress1());
+
+        textView = (TextView)view.findViewById(R.id.detailed_city);
+        textView.setText(provider.getAddresses().get(0).getCity());
+
+        textView = (TextView)view.findViewById(R.id.detailed_state);
+        textView.setText(provider.getAddresses().get(0).getState());
+
+        textView = (TextView)view.findViewById(R.id.detailed_country);
+        textView.setText(provider.getAddresses().get(0).getCountryName());
+
+        textView = (TextView)view.findViewById(R.id.detailed_zipcode);
+        textView.setText(provider.getAddresses().get(0).getPostalCode());
+
+        textView = (TextView)view.findViewById(R.id.detailed_phone);
+        textView.setText(provider.getAddresses().get(0).getPhone());
+
+        textView = (TextView)view.findViewById(R.id.detailed_fax);
+        textView.setText(provider.getAddresses().get(0).getFax());
     }
 
     @Override
