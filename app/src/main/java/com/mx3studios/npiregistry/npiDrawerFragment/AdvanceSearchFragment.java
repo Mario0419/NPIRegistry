097e2584678f757c0967209f5878534c49437eec
Mario Massad <Mario Massad>
2016-04-06 21:37:59 -0400

    working on advance search fragment

--- app/src/main/java/com/mx3studios/npiregistry/npiDrawerFragment/AdvanceSearchFragment.java
+++ app/src/main/java/com/mx3studios/npiregistry/npiDrawerFragment/AdvanceSearchFragment.java
@@ -1,6 +1,15 @@
 package com.mx3studios.npiregistry.npiDrawerFragment;
 
+import android.app.Dialog;
+import android.app.FragmentManager;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.mx3studios.npiregistry.R;
 
 /**
  * Created by Mario on 3/16/2016.
@@ -8,7 +17,23 @@ import android.support.v4.app.Fragment;
 public class AdvanceSearchFragment extends Fragment{
     int layout = 0;
 
+    private FragmentManager m;
+    private View view;
+    private Button searchButton;
+
     public int getLayoutRes() {
         return layout;
+    }
+
+    public static AdvanceSearchFragment newInstance() {
+        AdvanceSearchFragment fragment = new AdvanceSearchFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_advance_search, container, false);
+
+        return view;
     }
 }
